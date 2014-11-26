02e8517acbc79d06f3ce7122e3256abfb15c8845
lmuntaner <llorenc.muntaner@gmail.com>
2014-11-26 13:42:07 -0800

    Serialize json and both clicks on appointment modal

--- app/assets/javascripts/views/doctors/appointment_form.js
+++ app/assets/javascripts/views/doctors/appointment_form.js
@@ -3,13 +3,24 @@ Assisster.Views.AppointmentForm = Backbone.View.extend({
   className: "appointment-form-container",
   
   events: {
-    "click input[type='submit']": "cancel",
+    "click input[value='Cancel']": "cancel",
+		"click input[value='Save']": "save"
   },
   
   cancel: function (event) {
     event.preventDefault();
     this.$('#appointment-modal').modal('hide');
   },
+	
+	save: function (event) {
+		event.preventDefault();
+		$form = $(event.currentTarget).parent();
+		debugger
+    var startTime = date.toJSON();
+    var endTime = date.add(30, "m").toJSON();
+		var params = 
+		this.$('#appointment-modal').modal('hide');
+	},
   
   render: function () {
     var renderedContent = this.template();
