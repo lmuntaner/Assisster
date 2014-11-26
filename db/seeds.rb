11bf2a9f04e5bc5b3cb22250e72b0205f9b8e50c
Llorenc Muntaner <llorenc.muntaner@gmail.com>
2014-11-25 17:30:26 -0800

    seed one doctor

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Doctor.create(email: "william@wheat.com", password:"wheatbelly")
