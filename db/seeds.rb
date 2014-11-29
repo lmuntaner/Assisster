dcc325317aee062f5d438a2ee90facfca5908608
lmuntaner <llorenc.muntaner@gmail.com>
2014-11-29 11:05:42 -0800

    Improved seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,16 @@
 first_doctor = Doctor.create!(email: "william@wheat.com", password:"wheatbelly")
 
 now = DateTime.now
+zone = ActiveSupport::TimeZone.new("Pacific Time (US & Canada)")
                            
 5.times do |i|
-  appointment = now.next_day(i).noon.in(7200 * i);
-  first_doctor.appointments.create(title: Faker::Hacker.say_something_smart,
-                             email: Faker::Internet.email,
-                             fname: Faker::Name.first_name,
-                             lname: Faker::Name.last_name,
-                             startTime: appointment,
-                             endTime: appointment.in(3600))
+  5.times do |j|
+    appointment = now.next_day(i).midnight.in(7200 * j);
+    first_doctor.appointments.create(title: Faker::Hacker.say_something_smart,
+                               email: Faker::Internet.email,
+                               fname: Faker::Name.first_name,
+                               lname: Faker::Name.last_name,
+                               startTime: appointment,
+                               endTime: appointment.in(3600))
+  end
 end
