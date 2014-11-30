873f2d73f13812bebbeb7916c25c8d691a2e3963
lmuntaner <llorenc.muntaner@gmail.com>
2014-11-29 21:02:52 -0800

    just in case commit

--- app/assets/javascripts/views/patients/choose_time.js
+++ app/assets/javascripts/views/patients/choose_time.js
@@ -6,11 +6,10 @@ Assisster.Views.ChooseTime = Backbone.View.extend({
 		this.date = options.date;
 		this.collection = new Assisster.Collections.Appointments();
 		this.collection.getDateAppointments(this.date, this.model);
-		this.listenTo(this.collection, "add", this.render)
+		this.listenTo(this.collection, "add", this.render);
 	},
 	
 	render: function () {
-		debugger;
 		var renderedContent = this.template({
 			date: this.date
 		});
