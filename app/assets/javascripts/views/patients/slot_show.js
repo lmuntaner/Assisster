f59633418dbaa99ace7fbb5056c6ae1340f38cb7
lmuntaner <llorenc.muntaner@gmail.com>
2014-12-01 16:48:19 -0800

    Patients can set email, fname and lname when creating a new appoitment

--- app/assets/javascripts/views/patients/slot_show.js
+++ app/assets/javascripts/views/patients/slot_show.js
@@ -2,18 +2,30 @@ Assisster.Views.SlotShow = Backbone.View.extend({
 	template: JST["patients/slot_confirm"],
 	
 	events: {
-		"click button": "createAppointment"
+		"submit form": "createAppointment"
 	},
 	
 	createAppointment: function (event) {
-		this.model.set({
-			'doctor_id': window.Doctor.id,
-		});
-		this.model.save({}, {
-			success: function () {
-				Backbone.history.navigate("success", { trigger: true });				
-			}
-		})
+		event.preventDefault();
+		var params = $(event.currentTarget).serializeJSON().appointment;
+		debugger;
+		if (this.validateForm(params)) {
+	    var view = this;
+			var appointmentParams = {
+				email: params.email,
+				fname: params.fname,
+				lname: params.lname,
+				doctor_id: window.Doctor.id
+	    };
+	    this.model.save(appointmentParams, {
+	      success: function (model) {
+	        Backbone.history.navigate("success", { trigger: true });
+	      }
+	    })				
+		} else {
+			var $errors = $('<span>').text('Error!').addClass('error');
+			this.$('div.errors').prepend($errors);
+		}
 	},
 	
 	render: function () {
@@ -23,5 +35,13 @@ Assisster.Views.SlotShow = Backbone.View.extend({
 		this.$el.html(renderedContent);
 		
 		return this;
+	},
+	
+	validateForm: function (params) {
+		return this.validateEmail(params.email);
+	},
+	
+	validateEmail: function (title) {
+		return title != "";
 	},
 })
