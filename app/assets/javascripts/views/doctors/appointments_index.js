b65298603703a7b3dbbdf10b62093d5e99d73ed5
lmuntaner <llorenc.muntaner@gmail.com>
2014-12-02 21:09:15 -0800

    Appointments list with search bar and styling

--- app/assets/javascripts/views/doctors/appointments_index.js
+++ app/assets/javascripts/views/doctors/appointments_index.js
@@ -2,6 +2,10 @@ Assisster.Views.AppointmentsIndex = Backbone.CompositeView.extend({
 	template: JST['doctors/appointments_index'],
 	className: "appointments-index",
 	
+	events: {
+		"click tr": "showForm"
+	},
+	
   initialize: function (options) {
 		this.setApppointmentsSubviews();
 		this.listenTo(this.collection, "sync add remove", this.render);
@@ -39,6 +43,24 @@ Assisster.Views.AppointmentsIndex = Backbone.CompositeView.extend({
 		this.collection.each(function (appointment) {
 			view.addAppointmentSubview(appointment);
 		});
+	},
+	
+	showForm: function (event) {
+		var coordinates = [event.clientX, event.clientY];
+		var id = $(event.currentTarget).data('id');
+		var appointment = this.collection.get(id);
+		
+		if (this.appointmentForm) {
+			this.appointmentForm.remove();
+		}
+		
+    this.appointmentForm = new Assisster.Views.AppointmentForm({
+      collection: this.collection,
+			model: appointment,
+			coordinates: coordinates,
+    });
+		
+		this.$el.append(this.appointmentForm.render().$el);
 	},
 })
 
