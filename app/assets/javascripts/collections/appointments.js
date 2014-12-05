946dca8645adf8a54afea038aa43c8b1ff3aea39
lmuntaner <llorenc.muntaner@gmail.com>
2014-12-04 17:28:51 -0800

    Just in case commit

--- app/assets/javascripts/collections/appointments.js
+++ app/assets/javascripts/collections/appointments.js
@@ -67,16 +67,43 @@ Assisster.Collections.Appointments = Backbone.Collection.extend({
 		return appointments.slice(num_appointments - n, num_appointments);
 	},
 	
+	getFreeTimes: function (officeHours, appointments) {
+		var freeTimes = [];
+		officeHours.forEach(function (officeHour) {
+			var startTime = moment.utc(officeHour.get('startTime'));
+			var officeHourEndTime = moment.utc(officeHour.get('endTime'));
+			appointments.forEach(function (appointment) {
+				var endTime = moment.utc(appointment.get('startTime'));
+				if (endTime <= officeHourEndTime && endTime > startTime) {
+					var newFreeTime = new Assisster.Models.Appointment({
+						title: "Free Time",
+						startTime: startTime.clone(),
+						endTime: endTime.clone()
+					});
+					freeTimes.push(newFreeTime);
+				}
+				startTime = moment.utc(appointment.get('endTime'));
+			});
+		})
+		
+		return freeTimes;
+	},
+	
 	todaysAppointments: function () {
 		var todaysAppointments = this.select(function(appointment) {
 			return appointment.today() && !appointment.get('office_hour');
 		});
-		// var onlytodaysOfficeHours = this.select(function(appointment) {
-		// 	return appointment.get('office_hour');
-		// })
+		// var todaysOfficeHours = this.select(function(appointment) {
+		// 	return appointment.today() && appointment.get('office_hour');
+		// });
+		// var freeTimes = this.getFreeTimes(todaysOfficeHours, todaysAppointments);
 		var todaySorted = todaysAppointments.sort(function (a, b) {
-			return b.startTime() - a.startTime();				
+			return b.startTime() - a.startTime();
 		});
+		// var todayTotal = todaysAppointments.concat(freeTimes);
+		// var todaySorted = todayTotal.sort(function (a, b) {
+		// 	return b.startTime() - a.startTime();
+		// });
 		return todaySorted;
 	},
 });
