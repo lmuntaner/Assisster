54242440a2375c043e7753660a3c07ebef3e6279
lmuntaner <llorenc.muntaner@gmail.com>
2015-01-02 10:50:16 +0100

    form validation in js

--- app/assets/javascripts/views/patients/slot_show.js
+++ app/assets/javascripts/views/patients/slot_show.js
@@ -2,51 +2,81 @@ Assisster.Views.SlotShow = Backbone.View.extend({
 	template: JST["patients/slot_show"],
 	
 	events: {
+		"change input": "validateForm",
 		"click form>button": "createAppointment"
+	},
+	
+	initialize: function() {
+		// this.$errorDiv = $("div").addClass("alert alert-danger").attr("role", "alert");
 	},
 	
 	createAppointment: function (event) {
 		event.preventDefault();
 		var params = $(event.currentTarget).parent().serializeJSON().appointment;
-		if (this.validateForm(params)) {
-	    var view = this;
-			var appointmentParams = {
-				email: params.email,
-				fname: params.fname,
-				lname: params.lname,
-				country_code: params.countrycode,
-				phone_number: params.phoneNumber,
-				doctor_id: window.Doctor.id
-	    };
-	    this.model.save(appointmentParams, {
-	      success: function (model) {
-	        Backbone.history.navigate("success", { trigger: true });
-	      }
-	    })				
-		} else {
-			var $errors = $('<span>').text(this.errors).addClass('error');
-			this.$('div.errors').prepend($errors);
-		}
+    var view = this;
+		var appointmentParams = {
+			email: params.email,
+			fname: params.fname,
+			lname: params.lname,
+			country_code: params.countrycode,
+			phone_number: params.phoneNumber,
+			doctor_id: window.Doctor.id
+    };
+    this.model.save(appointmentParams, {
+      success: function (model) {
+        Backbone.history.navigate("success", { trigger: true });
+      }
+    });
 	},
 	
 	render: function () {
 		var renderedContent = this.template({
-			appointment: this.model,
+			appointment: this.model
 		});
 		this.$el.html(renderedContent);
 		
 		return this;
 	},
 	
-	validateForm: function (params) {
-		return this.validateEmail(params.email);
+	validateForm: function () {
+		if (this.validateEmail() && this.validateFname() && this.validateLname()) {
+			this.$('button').prop('disabled', false);
+		} else {
+			// this.$('.form-inputs').prepend(this.$errorDiv);
+			this.$('button').prop('disabled', true);
+		}
 	},
 	
-	validateEmail: function (email) {
-		if (email === '') {
-			this.errors = "Please enter your email!";
+	validateEmail: function () {
+		var $email = this.$("input[type='email']");
+		if ($email.val() === '') {
+			$email.addClass("required");
 			return false;
+		} else {
+			$email.removeClass("required");
+			return true;
 		}
-		return true;
+	},
+	
+	validateFname: function () {
+		var $fname = this.$("input[name='appointment[fname]']");
+		if ($fname.val() === '') {
+			$fname.addClass("required");
+			return false;
+		} else {
+			$fname.removeClass("required");
+			return true;
+		}
+	},
+	
+	validateLname: function () {
+		var $lname = this.$("input[name='appointment[lname]']");
+		if ($lname.val() === '') {
+			$lname.addClass("required");
+			return false;
+		} else {
+			$lname.removeClass("required");
+			return true;
+		}
 	},
 })
