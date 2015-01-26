f179020e89539302f296352d89c162e0a7d8ece4
lmuntaner <llorenc.muntaner@gmail.com>
2015-01-26 12:31:39 +0100

    working on combined search

--- app/assets/javascripts/views/doctors/search_view.js
+++ app/assets/javascripts/views/doctors/search_view.js
@@ -3,7 +3,9 @@ Assisster.Views.SearchView = Backbone.View.extend({
 	className: "col-xs-12 search-container",
 	
 	events: {
-		"keyup #email-search": "emailSearch"
+		"keyup #email-search": "emailSearch",
+		"keyup #fname-search": "fnameSearch",
+		"keyup #lname-search": "lnameSearch"
 	},
 	
 	initialize: function (options) {
@@ -14,18 +16,39 @@ Assisster.Views.SearchView = Backbone.View.extend({
 	},
 	
 	emailSearch: function (event) {
-		var value = $(event.currentTarget).val();
-		this.email = value;
-		this.results = this.collection.filter( function (appointment) {
-			return appointment.escape('email').search(value) !== -1;
-		});
+		this.email = $(event.currentTarget).val();
+		
+		this.search();
 		
 		this.render();
-		var strLength= value.length * 2;
+		var strLength = this.email.length * 2;
 
-		this.$("#email-search").focus();
 		this.$("#email-search")[0].setSelectionRange(strLength, strLength);
 		this.$("#email-search").focus();
+	},
+	
+	fnameSearch: function (event) {
+		this.fname = $(event.currentTarget).val();
+		
+		this.search();
+		
+		this.render();
+		var strLength = this.fname.length * 2;
+
+		this.$("#fname-search")[0].setSelectionRange(strLength, strLength);
+		this.$("#fname-search").focus();
+	},
+	
+	lnameSearch: function (event) {
+		this.lname = $(event.currentTarget).val();
+		
+		this.search();
+		
+		this.render();
+		var strLength = this.lname.length * 2;
+
+		this.$("#lname-search")[0].setSelectionRange(strLength, strLength);
+		this.$("#lname-search").focus();
 	},
 	
 	render: function () {
@@ -38,6 +61,14 @@ Assisster.Views.SearchView = Backbone.View.extend({
 		this.$el.html(renderedContent);
 		
 		return this;
+	},
+	
+	search: function () {
+		this.results = this.collection.filter( function (appointment) {
+			return appointment.escape('email').search(this.email) !== -1 && 
+						 appointment.escape('fname').search(this.fname) !== -1 &&
+						 appointment.escape('lname').search(this.lname) !== -1;
+		});
 	},
 	
 })
