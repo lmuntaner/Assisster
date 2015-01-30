a6621063696dec23888e5636cb60dcab3e7e1e06
lmuntaner <llorenc.muntaner@gmail.com>
2015-01-30 09:39:58 +0100

    sms form validating presence

--- app/assets/javascripts/views/doctors/send_sms.js
+++ app/assets/javascripts/views/doctors/send_sms.js
@@ -23,22 +23,63 @@ Assisster.Views.SendSmsView = Backbone.View.extend({
 	sendSms: function (event) {
 		event.preventDefault();
 		var params = $(event.currentTarget).parent().serializeJSON().sms;
-		var phoneNumber = params.countrycode + params.phoneNumber;
-		var url = "/api/send_messages";
-		var sms_data = {
-			phone_number: phoneNumber,
-			message: params.message,
-		};
-		var view = this;
-		$.ajax({
-		  url: url,
-		  type: "POST",
-		  data: {
-		    sms: sms_data
-		  },
-			success: function () {
-		    view.resetForm();
-		  }
-		});
-	}
+		if (this.validateParams(params)) {
+			var phoneNumber = params.countrycode + params.phoneNumber;
+			var url = "/api/send_messages";
+			var sms_data = {
+				phone_number: phoneNumber,
+				message: params.message,
+			};
+			var view = this;
+			$.ajax({
+			  url: url,
+			  type: "POST",
+			  data: {
+			    sms: sms_data
+			  },
+				success: function () {
+			    view.resetForm();
+			  }
+			});
+		}
+	},
+	
+	validateCountry: function (countrycode) {
+		if (countrycode.length > 0) {
+			this.$("div.sms-countrycode").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.sms-countrycode").addClass("has-error");
+			return false;
+		}
+	},
+	
+	validateMessage: function (message) {
+		if (message.length > 0) {
+			this.$("div.sms-message").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.sms-message").addClass("has-error");
+			return false;
+		}
+	},
+	
+	validatePhoneNumber: function (phoneNumber) {
+		if (phoneNumber.length > 0) {
+			this.$("div.sms-phoneNumber").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.sms-phoneNumber").addClass("has-error");
+			return false;
+		}
+	},
+	
+	validateParams: function (params) {
+		var validated = true;
+		if (!this.validateCountry(params.countrycode)) validated = false;
+		if (!this.validatePhoneNumber(params.phoneNumber)) validated = false;
+		if (!this.validateMessage(params.message)) validated = false;
+		
+		return validated;
+	},
 })
