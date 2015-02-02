4aaa75da2c6cd35c47a2a9eb6df1916b5a1f990f
lmuntaner <llorenc.muntaner@gmail.com>
2015-02-02 19:20:19 +0100

    service form validation

--- app/assets/javascripts/views/doctors/new_service_form.js
+++ app/assets/javascripts/views/doctors/new_service_form.js
@@ -8,8 +8,10 @@ Assisster.Views.NewServiceForm = Backbone.View.extend({
 	
 	createService: function (event) {
 		var serviceParams = $(event.currentTarget).parent().serializeJSON().service;
-		this.collection.create(serviceParams, {wait: true});
-		this.resetForm();
+		if (this.validateParams(serviceParams)) {
+			this.collection.create(serviceParams, {wait: true});
+			this.resetForm();			
+		}
 	},
   
   render: function () {
@@ -22,5 +24,44 @@ Assisster.Views.NewServiceForm = Backbone.View.extend({
 	resetForm: function () {
 		this.$("input").val("");
 		this.$("textarea").val("")
-	}
+	},
+	
+	validateDescription: function (description) {
+		if (description.length > 0) {
+			this.$("div.service-description-form").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.service-description-form").addClass("has-error");
+			return false;
+		}
+	},
+
+	validateDuration: function (duration) {
+		if (/^\d+$/.test(duration) && duration > 0 && duration < 1440) {
+			this.$("div.service-duration").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.service-duration").addClass("has-error");
+			return false;
+		}
+	},
+	
+	validateTitle: function (title) {
+		if (title.length > 0) {
+			this.$("div.service-title").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.service-title").addClass("has-error");
+			return false;
+		}
+	},
+
+	validateParams: function (params) {
+		var validated = true;
+		if (!this.validateDescription(params.description)) validated = false;
+		if (!this.validateDuration(params.duration_min)) validated = false;
+		if (!this.validateTitle(params.title)) validated = false;
+	
+		return validated;
+	},
 })
--- app/assets/javascripts/views/doctors/service_form.js
+++ app/assets/javascripts/views/doctors/service_form.js
@@ -36,11 +36,52 @@ Assisster.Views.ServiceForm = Backbone.View.extend({
 	updateService: function (event) {
 		var view = this;
 		var serviceParams = $(event.currentTarget).parent().serializeJSON().service;
-		this.model.save(serviceParams, {
-			success: function(service) {
-				view.collection.add(service, {merge: true});
-				view.closeForm();
-			}
-		});
+		if (this.validateParams(serviceParams)) {
+			this.model.save(serviceParams, {
+				success: function(service) {
+					view.collection.add(service, {merge: true});
+					view.closeForm();
+				}
+			});			
+		}
+	},
+	
+	validateDescription: function (description) {
+		if (description.length > 0) {
+			this.$("div.service-description-form").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.service-description-form").addClass("has-error");
+			return false;
+		}
+	},
+
+	validateDuration: function (duration) {
+		if (/^\d+$/.test(duration) && duration > 0 && duration < 1440) {
+			this.$("div.service-duration").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.service-duration").addClass("has-error");
+			return false;
+		}
+	},
+	
+	validateTitle: function (title) {
+		if (title.length > 0) {
+			this.$("div.service-title").removeClass("has-error");
+			return true;
+		} else {
+			this.$("div.service-title").addClass("has-error");
+			return false;
+		}
+	},
+
+	validateParams: function (params) {
+		var validated = true;
+		if (!this.validateDescription(params.description)) validated = false;
+		if (!this.validateDuration(params.duration_min)) validated = false;
+		if (!this.validateTitle(params.title)) validated = false;
+	
+		return validated;
 	},
 })
