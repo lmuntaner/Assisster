62769097f66cc984cad08ad0f31db24dab1fa3c1
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2015-04-04 11:12:20 +0200

    home template js and css added in vendor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+%w( patients doctors doctor_web_settings ).each do |controller|
+  Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
+end
+
