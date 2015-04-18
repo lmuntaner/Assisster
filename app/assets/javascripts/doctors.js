55b3aad6403397c6fd420eca6eb9d24e00b2263d
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2015-04-18 11:00:59 +0200

    requiring assets on doctors.js

---
+++ app/assets/javascripts/doctors.js
@@ -0,0 +1,23 @@
+//
+//
+//= require jquery.serializejson
+//= require jquery.timepicker
+//= require jquery.backstretch.min
+//= require bootstrap-datepicker
+//= require underscore
+//= require backbone
+//= require backbone.TriggerHappy
+//= require moment
+//= require fullcalendar
+//= require lang/es.js
+//= require pusher.min
+//= require list
+//= require bootstrap-notify
+//= require assisster.js
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+//= require phone_codes.js
+//= require status_spanish.js
