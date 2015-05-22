715967d9fcaf83f6c93aba72a1ef577d24d60865
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2015-05-22 14:32:29 +0200

    functionality doctor invitation input in home

--- app/controllers/api/doctor_invitations_controller.rb
+++ app/controllers/api/doctor_invitations_controller.rb
@@ -1,12 +1,12 @@
 class Api::DoctorInvitationsController < ApplicationController
 
 	def create
-		doctor_invitation_email = params.require(:doctor_invitation).permit(:email)
-		doctor_invitation = DoctorInvitation.new(email: doctor_invitation_email)
+		doctor_invitation_params = params.require(:doctor_invitation).permit(:email)
+		doctor_invitation = DoctorInvitation.new(doctor_invitation_params)
 
 		if doctor_invitation.save
 			doctor_invitation.send_creation_email
-			render json: doctor_invitation_email
+			render json: doctor_invitation
 		else
 			render json: doctor_invitation.errors.full_messages, status: :unprocessable_entity
 		end
